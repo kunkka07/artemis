fa0a20d10ec6f88eb513cd860af3d74c875ac855
kunkka19xx <nobita079x@gmail.com>
2026-01-28 16:52:15 +0900

    edit flake.nix

--- flake.nix
+++ flake.nix
@@ -34,12 +34,6 @@
             shellHook = ''
               echo "Dev environment ready for ${system}"
               echo "Building project with graalvm"
-              cd cli
-              ./gradlew nativeCompile --no-configuration-cache
-              echo "Finished building project with graalvm, go to the build dir"
-              cd app/build/native/nativeCompile
-              chmod +x app
-              echo "You can run the app now"
             '';
           };
         }
